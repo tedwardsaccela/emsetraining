bbf79d373d44136904b0b6280e493771d995958e
angelogab <angelo_francisco@yahoo.com>
2017-04-04 09:27:26 -0700

    Exercise 2

--- ASOLIVA/Scripts/Event/ASA;BUILDING!RESIDENTIAL!ELECTRICAL!NA.js
+++ ASOLIVA/Scripts/Event/ASA;BUILDING!RESIDENTIAL!ELECTRICAL!NA.js
@@ -1,12 +1,5 @@
-try{
-	
 //Start Group Exercise 2
-//@TODO - Clarify with client any criteria
-	
-	addStdCondition("Building Permit","Utility Locate");
+
+addStdCondition("Building Permit", "Utility Locate");
 
 //End Group Exercise 2
-
-} catch (err){
-	logDebug("A JavaScript Error occured in ASA:BUILDING/RESIDENTIAL/ELECTRICAL/NA: " + err.message + " In Line " + err.lineNumber);
-}
